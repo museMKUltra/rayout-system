dd934e2c21cf6efae598dfbf4495f8986eab785d
Roller Lo <jasonbh02@gmail.com>
2021-07-07 21:40:21 +0800

    refactor margin multiplier

--- src/components/RayoutListInline.jsx
+++ src/components/RayoutListInline.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 import { color, layout, flexbox, space } from 'styled-system'
 
-function margin(times) {
+function multiplier(times) {
 	return ({ gapY = 0, gapX = 0 }) => `${times * gapY}px ${times * gapX}px`
 }
 
 const List = styled.div`
 	display: inline-block;
-  ${color}
+	${color}
 `
 
 List.Inline = styled.div`
 	display: inline-flex;
 	vertical-align: top;
 	flex-wrap: wrap;
-	margin: ${margin(-0.5)};
+	margin: ${multiplier(-0.5)};
+
 	${layout}
-  ${flexbox}
+	${flexbox}
   ${space}
   > * {
-		margin: ${margin(0.5)};
+		margin: ${multiplier(0.5)};
 	}
 `
 
