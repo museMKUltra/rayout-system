deaf388e615532fac5eb140a6cfcb30678f317ca
Roller Lo <jasonbh02@gmail.com>
2021-07-10 11:27:34 +0800

    keep elements clean for testing in app js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'styled-components'
 import styled from 'styled-components'
 import { layout } from 'styled-system'
 import theme from './configs/theme.js'
-import RayoutColumn from './components/RayoutColumn.jsx'
 
 const Div = styled.div`
 	background: lightgray;
@@ -14,17 +13,7 @@ const Div = styled.div`
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<RayoutColumn
-				contentMinWidth={''}
-				columnCount={''}
-				flowDirection={''}
-				verticalAlign={''}
-				gap={''}
-				gapX={''}
-				gapY={''}
-			>
-				<Div></Div>
-			</RayoutColumn>
+			<Div></Div>
 		</ThemeProvider>
 	)
 }
