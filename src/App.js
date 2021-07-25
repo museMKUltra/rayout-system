f6372dbcff05c31995ce621e8bcea694981f2942
Roller Lo <jasonbh02@gmail.com>
2021-07-25 16:50:10 +0800

    add components & stories of RayoutAlign

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
 import React from 'react'
 import './App.css'
 import styled from 'styled-components'
-import { position, layout } from 'styled-system'
-import RayoutAbsolute from './components/RayoutAbsolute.jsx'
-import RayoutList from './components/RayoutList.jsx'
-import RayoutListInline from './components/RayoutListInline.jsx'
+import { layout } from 'styled-system'
+import RayoutAlign from './components/RayoutAlign.jsx'
 
-const Box = styled.div`
+const Box = styled(RayoutAlign)`
 	background: darkgray;
-	${position}
 	${layout}
 `
 
 const Div = styled.div`
-	height: 100px;
-	width: 100px;
+	height: 80px;
 	background: lightgray;
-	${layout}
 `
 
 function App() {
 	return (
-		<Box position={'relative'} height={'400px'}>
-			<RayoutAbsolute paddingTop={10}>
-				<RayoutAbsolute.TopLeft>
-					<Div>top</Div>
-				</RayoutAbsolute.TopLeft>
-				<RayoutAbsolute.Left>
-					<Div>left</Div>
-				</RayoutAbsolute.Left>
-				<RayoutAbsolute.Right>
-					<Div>right</Div>
-				</RayoutAbsolute.Right>
-				<RayoutAbsolute.BottomRight>
-					<Div>bottom</Div>
-				</RayoutAbsolute.BottomRight>
-			</RayoutAbsolute>
-			<RayoutList>
-				<div>list</div>
-			</RayoutList>
-			<RayoutListInline>
-				<div>list inline</div>
-			</RayoutListInline>
+		<Box paddingTop={10} height={'400px'}>
+			<Div>align</Div>
 		</Box>
 	)
 }
---
+++ src/components/RayoutAlign.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import styled from 'styled-components'
+import theme from '../configs/theme.js'
+import { ThemeProvider } from 'styled-components'
+import { space, flexbox } from 'styled-system'
+import PropTypes from 'prop-types'
+
+const horizontalAlignMapping = {
+	default: '',
+	left: 'start',
+	center: 'center',
+	right: 'end',
+}
+
+const verticalAlignMapping = {
+	default: '',
+	top: 'start',
+	center: 'center',
+	bottom: 'end',
+}
+
+const Container = styled.div`
+	display: grid;
+	grid-template-columns: minmax(0, auto);
+	box-sizing: border-box;
+	${space}
+  ${flexbox}
+`
+
+function RayoutAlign({
+	paddingTop,
+	paddingBottom,
+	paddingLeft,
+	paddingRight,
+	horizontalAlign,
+	verticalAlign,
+	children,
+	...rest
+}) {
+	return (
+		<ThemeProvider theme={theme}>
+			<Container
+				pt={paddingTop}
+				pb={paddingBottom}
+				pl={paddingLeft}
+				pr={paddingRight}
+				justifyContent={horizontalAlignMapping[horizontalAlign]}
+				alignContent={verticalAlignMapping[verticalAlign]}
+				{...rest}
+			>
+				{children}
+			</Container>
+		</ThemeProvider>
+	)
+}
+
+RayoutAlign.propTypes = {
+	paddingTop: PropTypes.number,
+	paddingBottom: PropTypes.number,
+	paddingLeft: PropTypes.number,
+	paddingRight: PropTypes.number,
+	horizontalAlign: PropTypes.oneOf(['default', 'left', 'center', 'right']),
+	verticalAlign: PropTypes.oneOf(['default', 'top', 'center', 'bottom']),
+}
+
+RayoutAlign.defaultProps = {
+	paddingTop: 0,
+	paddingBottom: 0,
+	paddingLeft: 0,
+	paddingRight: 0,
+	horizontalAlign: 'default',
+	verticalAlign: 'default',
+}
+
+export default RayoutAlign
